899e7938bda5c069c75e7c9811afe26911e59259
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2024-03-14 17:38:36 -0500

    REU Tab basic content

--- REUTab.php
+++ REUTab.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<link rel="Stylesheet" href="styles.css">
+	<link rel="stylesheet" href="styles.css">
 	<title>Internship Database - REUs</title>
 </head>
 
@@ -17,6 +17,27 @@
 	</header>
 	
 	<main>
+		<h2>Why</h2>
+		<section id="homePageMain">
+			<p>REUs, or Research Experiences for Undergraduates, are programs funded by the NSF to help undergraduates
+                become interested in the prospect of research. They're also, more importantly, <b>more likely to accept underclassmen</b>
+                than traditional internships.
+            </p>
+			<h2>What</h2>
+            <p>REUs are 10 week programs hosted at universities across the country. You're assigned a research advisor
+                upon your acceptance, and that advisor has some kind of real-world project they'll guide you to work through, helping
+                you learn whatever you need to know along the way. At the end of the program, you'll present your project and
+                research. Along the way, you'll get to meet a lot of new people, explore a new campus, and do work not strictly
+                limited to programming.
+            </p>
+            <h2>Where</h2>
+            <p>REUs are hosted primarily at larger universities, but can theoretically happen anywhere. Larger universities
+                tend to have larger programs, however.
+            </p>
+            <h2>How</h2>
+            <p>Unlike internships, REUs already have a <a href=https://www.nsf.gov/crssprgm/reu/list_result.jsp?unitid=5049>
+            complete, usable database</a> to aid you in your search. We'd highly recommend checking it out if you're interested.</p>
+		</section>
 		<footer>
 			Created by Andy Bernatow, Cole Bracken, Aidan Dunne, Owen Murphy &mdash; 2024.
 		</footer>

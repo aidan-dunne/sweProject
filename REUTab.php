07e3eab21ae6e3f52c560f67319c829341d02117
Aidan Dunne <aidanmdunne03@gmail.com>
2024-03-14 14:11:17 -0500

    basic webpage and stylesheet setup

---
+++ REUTab.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="Stylesheet" href="styles.css">
+	<title>Internship Database - REUs</title>
+</head>
+
+<body>
+	<header>
+		<h1>REUs</h1>
+		<nav>
+			<a href="index.php">Home</a>
+			<a href="pastInternships.php">Companies and Programs</a>
+			<a href="REUTab.php">REUs</a>
+		</nav>
+	</header>
+	
+	<main>
+		<footer>
+			Created by Andy Bernatow, Cole Bracken, Aidan Dunne, Owen Murphy &mdash; 2024.
+		</footer>
+	</main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="styles.css">
+	<title>Internship Database - Home</title>
+</head>
+
+<body>
+	<header>
+		<h1>Home</h1>
+		<nav>
+			<a href="index.php">Home</a>
+			<a href="pastInternships.php">Companies and Programs</a>
+			<a href="REUTab.php">REUs</a>
+		</nav>
+	</header>
+	
+	<main>
+		<h2>Welcome - your job search starts here!</h2>
+		<section id="homePageMain">
+			<p>Whether you're looking for an internship that fits your skillset, an REU in a research area that interests you, or a company at which
+			Truman students like you have had success at in the past, it can all be found here. </p>
+			<h3>Resources</h3>
+			<p>With our website, we aim to make the internship-searching process more accessible and less stressful for groups like
+			international students and underclassmen who may have a difficult time finding internships that will consider their
+			applications. Our internship database offers a range of easy-to-use search filters which will allow you to refine your internship search
+			and more efficiently find internships that are perfect for you. Additionally, our (placeholder name) page provides a list of companies that 
+			offer internship programs at which Truman students have been successful at in the past.</p>
+			<p>Our Research Experiences for Undergraduates (REU) page offers an explanation of what REUs are, why you may want to consider one
+			over an internship, and provides a link to the National Science Foundation's REU database.</p>
+			<h3>Whatever it is you're looking for, we hope we can help you find it!</h3>
+		</section>
+		<footer>
+			Created by Andy Bernatow, Cole Bracken, Aidan Dunne, Owen Murphy &mdash; 2024.
+		</footer>
+	</main>
+</body>
+</html>

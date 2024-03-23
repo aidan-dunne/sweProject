018e6dfef772e3363ce7cd594bfab58f40806cd4
Aidan Dunne <aidanmdunne03@gmail.com>
2024-03-23 13:05:01 -0500

    Added user profile button image

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 	<div class="headerTopBG"></div>
 	<header>
 		<h1>Home</h1>
+		<img src="images/profilePageIcon.png" class="profIcon"></img>
 		<nav id="mainNav">
 			<a href="index.php" class="currentPage">Home</a>
 			<a href="pastInternships.php">Companies and Programs</a>
@@ -24,17 +25,17 @@
 	
 	<main>
 		<section class="pageContentMain">
-			<h2>Welcome - your job search starts here!</h2>
+			<h2>Welcome &mdash; your job search starts here!</h2>
 			<p>Whether you're looking for an internship that fits your skillset, an REU in a research area that interests you, or a company at which
 			Truman students like you have had success at in the past, it can all be found here. </p>
 			<h3>Resources</h3>
 			<p>With our website, we aim to make the internship-searching process more accessible and less stressful for groups like
 			international students and underclassmen who may have a difficult time finding internships that will consider their
 			applications. Our internship database offers a range of easy-to-use search filters which will allow you to refine your internship search
-			and more efficiently find internships that are perfect for you. Additionally, our Companies and Programs page provides a list of companies 
-			that offer internship programs at which Truman students have been successful at in the past.</p>
-			<p>Our Research Experiences for Undergraduates (REU) page offers an explanation of what REUs are, why you may want to consider one
-			over an internship, and provides a link to the National Science Foundation's REU database.</p>
+			and more efficiently find internships that are perfect for you. Additionally, our <a href="pastInternships.php">Companies and Programs</a> 
+			page provides a list of companies that offer internship programs at which Truman students have been successful at in the past.</p>
+			<p>Our <a href="REUTab.php">Research Experiences for Undergraduates (REU)</a> page offers an explanation of what REUs are, why you 
+			may want to consider one over an internship, and provides a link to the National Science Foundation's REU database.</p>
 			<h3>Whatever it is you're looking for, we hope we can help you find it!</h3>
 		</section>
 		<footer>

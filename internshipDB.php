cd135e7a393b368063e7a78932409820ed235fac
Andy Bernatow <99744066+andy-bernato@users.noreply.github.com>
2024-03-24 18:52:42 -0500

    database linkage

--- internshipDB.php
+++ internshipDB.php
@@ -36,7 +36,37 @@
 			<p>[DATABASE INTERFACE PLACEHOLDER]</p>
 			<h2>Filters</h2>
 			<h3>International Student Filter</h3>
+			<p id="test"></p>
 		</section>
+		<script type="module">
+			  // Import the functions you need from the SDKs you need
+  				import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+  				import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-database.js";
+
+  				document.getElementById("test").innerHTML="Test";
+  				// Your web app's Firebase configuration
+  				const firebaseConfig = {
+   					apiKey: "AIzaSyCEs4fuVQHi2dwqnV6TJHSO1fZ6qx6kXc8",
+    				authDomain: "se-internship-database.firebaseapp.com",
+    				databaseURL: "https://se-internship-database-default-rtdb.firebaseio.com/",
+    				projectId: "se-internship-database",
+    				storageBucket: "se-internship-database.appspot.com",
+    				messagingSenderId: "520655988080",
+    				appId: "1:520655988080:web:573c2f7436e2acddf89bb5"
+ 				};
+
+  				// Initialize Firebase
+  				const app = initializeApp(firebaseConfig);
+
+  				const db = getDatabase(app);
+				const bookRef = ref(db, "book/Book1/Author");
+
+				onValue(bookRef, (snapshot) => {
+  					const data = snapshot.val();
+  					document.getElementById("test").innerHTML=(data);
+				});
+
+		</script>
 		<footer>
 			Created by Andy Bernatow, Cole Bracken, Aidan Dunne, <small>and</small> Owen Murphy <small>with help from</small> James Calder, Adi Shah,
 			<small>and</small> Paige Su &mdash; 2024.

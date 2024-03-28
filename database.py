1ccbbc800f25744b80eee38b86eee9eb30612539
Owen Murphy <111613450+owentm@users.noreply.github.com>
2024-03-27 19:06:26 -0500

    file that extracts data and sends it to the db

--- database.py
+++ database.py
@@ -2,6 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import json
+import scrape
 
 #Fetch the service account key JSON file contents
 cred = credentials.Certificate('se-internship-database-firebase-adminsdk-10szo-64b63bdb76.json')
@@ -10,8 +11,18 @@ firebase_admin.initialize_app(cred, {
     'databaseURL': "https://se-internship-database-default-rtdb.firebaseio.com/"
 })
 
-ref = db.reference("/")
 
-with open("testingdoc.json", "r") as f:
+
+
+
+
+
+ref = db.reference("/")
+users_ref = ref.child('user')
+
+with open("database.json", "r") as f:
     file_contents = json.load(f)
-ref.set(file_contents)
+#ref.set(file_contents)
+
+db_ref = ref.child('internships')
+db_ref.set(file_contents)

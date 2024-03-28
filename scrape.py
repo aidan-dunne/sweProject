ead35a750dc6264bc03a17af65042b5a57c65350
Owen Murphy <111613450+owentm@users.noreply.github.com>
2024-03-27 19:05:25 -0500

    formatted, fixed bugs, added functionality
    I formatted the code, and fixed the issues that made it so the flags for citizenship and underclassman status wouldn't update. I also fixed the issue where not all of the internships were added to the return value. I also added a parameter that returns the link for the internship. Important note that i commented out the code that sent everything to the database; I did this so testing with the database could be done with a constant json file, to make testing easier.

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import json
 
 
 def jobScrape():
+    
     params = {
         'api_key': '5121d893caf92b7ccd439e36f6643ed5d781a31971d9dffe702e21d5003fc2ba',
         # https://serpapi.com/manage-api-key
@@ -11,59 +12,72 @@ def jobScrape():
         'hl': 'en',  # language of the search
         'q': 'Computer Science Internships',  # search query
     }
+
     objectList = []
+
     # variables to store all database information
+
     jobTitle = ''
     companyName = ''
     location = ''
     desc = ''
+    link = ''
     # citizen determines if the job needs the applicant to be a US citizen
     citizen = False
     # underclassman determines if the applicant can be a freshman or sophomore
     underclassman = False
 
-    jobList =[]
-    companyList =[]
-    locationList=[]
-    underList=[]
-    citizenList=[]
-
     search = GoogleSearch(params).get_dict()['jobs_results']  # where data extraction happens on the SerpApi backend
-
+    dummy = 0
     # going through each job...
     for items in search:
+
         # check to see if the job is necessarily an internship
         # google job search is not amazing with this,
         # so i'll make sure to filter the actual jobs out and just output the internships or co ops
+
+        # these are parameters that will be used later
+        checkI = 0
+        checkU = 0
+
         if ('intern' in items.get('title').lower() or 'internship' in items.get('title').lower()
-                or 'co-op' in items.get('title').lower()):
+                or 'internships' in items.get('title').lower or 'co-op' in items.get('title').lower()):
+
             # if so, extract title, name, and location
+
             jobTitle = items.get('title')
-            jobList.append(jobTitle)
-
             companyName = items.get('company_name')
-            companyList.append(companyName)
-
             location = items.get('location')
-            locationList.append(location)
-
             qualifications = items.get('job_highlights')[0].get('items')
-            # now, check the qualifications to see if the student needs to be a citizen or can be an underclassman\
+            link = items.get('related_links')[0].get('link')
+
+            # now, check the qualifications to see if the student needs to be a citizen or can be an underclassman
+            # if any of the qualifications have either "citizenship" or anything referring to
+            # needing to be an upperclassman, add one to the count. If the count
+            # is anything but 0, the flag is marked as FALSE; meaning
+            # that you either must be a citizen or an upperclassman
+
             for x in range(len(qualifications)):
                 if 'Citizenship' in qualifications[x] or 'Citizen' in qualifications[x]:
-                    citizen = False
-                    citizenList.append(citizen)
+                    checkI += 1
                 else:
-                    citizen = True
-                    citizenList.append(citizen)
+                    checkI += 0
 
                 if 'Junior' in qualifications[x] or 'Senior' in qualifications[x]:
-                    underclassman = False
-                    underList.append(underclassman)
+                    checkU += 1
 
                 else:
-                    underclassman = True
-                    underList.append(underclassman)
+                    checkU += 0
+
+            if checkI != 0:
+                citizen = False
+            else:
+                citizen = True
+
+            if checkU != 0:
+                underclassman = False
+            else:
+                underclassman = True
 
 
             # printing out for testing reasons
@@ -73,7 +87,10 @@ def jobScrape():
             print(qualifications)
             print(citizen)
             print(underclassman)
+            print(link)
             print(' ')
+
+            # after that, make a json formatted object to write to the json file
 
             object = {
                 'job name': jobTitle,
@@ -82,14 +99,18 @@ def jobScrape():
                 'citizenship': citizen,
                 'underclassman': underclassman
             }
+            # then, make a list of json writeable text
+
             objectList.append(object)
 
 
         else:
-            break
+            dummy= 0
 
-    with open('database.json', 'w') as f:
-        json.dump(objectList, f, indent= 2)
+
+    # finally, write the list of jsonable text to the json code to send to the file
+   # with open('database.json', 'w') as f:
+      #  json.dump(objectList, f, indent= 2)
 
 
     return

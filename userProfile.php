368d37ea204b1d68417cacc37b667859ea259094
Aidan Dunne <aidanmdunne03@gmail.com>
2024-04-04 16:11:54 -0500

    user profile tests

--- userProfile.php
+++ userProfile.php
@@ -7,11 +7,10 @@
 	If the form has been submitted (user has successfully logged in/signed up), a session variable indicating that the user has logged in is set to true.
 	This variable is later used to load the user's profile page rather than the login/signup pages.
 	
-	Additionally, a "usernameDisplay" session variable is set which allows the user's username to be easily displayed in the profile page's header.
+	Additionally, a "name_of_user" session variable is set which allows the user's name to be easily displayed in the profile page's header.
 	*/
-	if(isset($_POST['signUpSubmit'])) {
+	if(isset($_POST['signUpSubmit']) or isset($_POST['logInSubmit'])) {
 		$_SESSION['loggedIn'] = true;
-		$_SESSION['usernameDisplay'] = $_POST['usernameSU'];
 		$_SESSION['name_of_user'] = $_POST['nameSU'];
 	}
 ?>
@@ -34,8 +33,8 @@
 		<!-- User's username will be retrieved and displayed in header if the user has logged in -->
 		<?php
 			if ($_SESSION['loggedIn']) {
-				$usernameDisplay = $_SESSION['name_of_user'];
-				echo "<h1>Profile - $usernameDisplay</h1>";
+				$nameDisplay = $_SESSION['name_of_user'];
+				echo "<h1>Profile - $nameDisplay</h1>";
 			}
 			else { //A default profile page header will be displayed if the user has not logged in
 				echo "<h1>Profile</h1>";
@@ -78,6 +77,7 @@
 					//Building the signup form which will be validated with javascript later
 					echo <<< MULTILINE
 						<form method='post' action='userProfile.php' id='signUp'>
+							<input type='hidden' id='formLoaded' value='SU'>
 							<input type='text' name='nameSU' id='nameSU' placeholder='Name'>
 							<input type='text' name='usernameSU' id='usernameSU' placeholder='Username'>
 							<input type='password' name='passwordSU' id= 'passwordSU' placeholder='Password'>
@@ -88,7 +88,14 @@
 				
 				if (isset($_POST['loadPageLogIn'])) {
 					//Building the login form which will be validated by javascript later
-					echo "<p>testLogIn</p>"; //(I haven't actually built this form)
+					echo <<< MULTILINE
+						<form method='post' action='userProfile.php' id='logIn'>
+							<input type='hidden' id='formLoaded' value='LI'>
+							<input type='text' name='usernameLI' id='usernameLI' placeholder='Username'>
+							<input type='password' name='passwoLI' id= 'passwordLI' placeholder='Password'>
+							<input type='submit' name='logInSubmit' value='Log In'>
+						</form>
+					MULTILINE;
 				}
 			}
 		?>
@@ -117,17 +124,17 @@
 			const snapshot = await get(ref(db, "users"));
 			
 			/*
-			When a user attempts to sign up for the first time, this function will compare the entered username to all other usernames stored in the
-			database to ensure that the entered username is available (does not match an existing username).
+			When a user attempts to sign up or log in, this function will compare the entered username to all other usernames stored in the database.
 			
-			Returns true if the entered username is unavailable (matches a pre-existing username), returns false if the entered username is available.
+			Returns true if the entered username matches an existing username in the database (username is unavailable when signing up/username is
+			correct whenn logging in), returns false otherwise (username is available when signing up/username is incorrect when logging in).
 			*/
 			function determineMatch (userEntered) {
 				let flag = false;
 				
 				snapshot.forEach(function(childSnapshot) {
 					let userCompare = childSnapshot.child("username").val();
-					if (userEntered ==  userCompare) {
+					if (userEntered == userCompare) {
 						flag = true;
 					}
 				});
@@ -135,31 +142,77 @@
 				return flag;
 			}
 			
+			//When a user attempts to log in, this function will compare the entered password to the password associated with the user's entered username.
+			//Returns true if the database-stored and entered passwords match, returns false otherwise;
+			function correctPassword (userEntered, passEntered) {
+				let flag = false;
+				
+				let passCompare = snapshot.child(userEntered).child("password").val();
+				if (passEntered == passCompare) {
+					flag = true;
+				}
+				
+				return flag;
+			}
+			
 			//Flag variable that indicates whether the username a user entered when signing up is available
 			let matchFlagSU = false;
 			
-			//Getting a reference to the signup form (built earlier in php) and assigning it an event listener which listens the "form submitted" event
-			let signInForm = document.getElementById("signUp");
-			signInForm.addEventListener("submit", function (event) { //When the signup form is submitted, check entered username availability
-				let usernameSU = document.getElementById("usernameSU").value;
-				
-				//Calling the determineMatch function to check if the user-entered username is available
-				matchFlagSU = determineMatch(usernameSU);
-				if (matchFlagSU) { //If username is unavailable, send alert and prevent form from being submitted
-					alert("Error: that username is already in use!");
-					event.preventDefault();
-				}
-				else { //Otherwise, pull other fields from submitted form and write them to the database
-					let nameSU = document.getElementById("nameSU").value;
-					let passwordSU = document.getElementById("passwordSU").value;
+			//Flag variables that indicate whether the username/password entered by a user when logging in are correct (match database data)
+			let matchUserLI = false;
+			let matchPassLI = false;
+			
+			let formLoaded = document.getElementById("formLoaded").value;
+			//alert(formLoaded);
+			
+			if (formLoaded == "SU") {
+				//Getting a reference to the signup form (built earlier in php) and assigning it an event listener which listens for the form being submitted
+				let signInForm = document.getElementById("signUp");
+				signInForm.addEventListener("submit", function (event) { //When the signup form is submitted, check entered username availability
+					let usernameSU = document.getElementById("usernameSU").value;
+					
+					//Calling the determineMatch function to check if the user-entered username is available
+					matchFlagSU = determineMatch(usernameSU);
+					if (matchFlagSU) { //If username is unavailable, send alert and prevent form submission
+						alert("Error: that username is already in use!");
+						event.preventDefault();
+					}
+					else { //Otherwise, pull other fields from submitted form and write them to the database
+						let nameSU = document.getElementById("nameSU").value;
+						let passwordSU = document.getElementById("passwordSU").value;
 
-					set(ref(db, "users/"+usernameSU), {
-						username: usernameSU,
-						password: passwordSU,
-						name_of_user: nameSU,
-					});
-				}
-			});
+						set(ref(db, "users/"+usernameSU), {
+							username: usernameSU,
+							password: passwordSU,
+							name_of_user: nameSU,
+						});
+					}
+				});
+			}
+			else {
+				//Getting a reference to the login form (built earlier in php) and assigning it an event listener which listens for the form being submitted
+				let logInForm = document.getElementById("logIn");
+				logInForm.addEventListener("submit", function (event) {
+					let usernameLI = document.getElementById("usernameLI").value;
+					let passwordLI = document.getElementById("passwordLI").value;
+					
+					alert(usernameLI);
+					
+					//Calling the determineMatch function to check if the user-entered username is a valid login username (present in our database)
+					matchUserLI = determineMatch(usernameLI);
+					if (matchUserLI) { //If entered username matches with a username in the database, compare passwords for equality
+						passwordLI = correctPassword(usernameLI, passwordLI);
+						if (!passwordLI) { //If entered and stored passwords do not match, send alert and prevent form submission
+							alert("Error: username or password is incorrect!");
+							event.preventDefault();
+						}
+					}
+					else { //If entered username is not present in the database, send alert and prevent form submission
+						alert("Error: username or password is incorrect!");
+						event.preventDefault();
+					}
+				});
+			}
 			
 			//Syntax for writing to database
 			/*
